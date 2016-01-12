e689b2580bb0e732e6e99ced3c295fff11dce76a
pavel.kuznetsov <pavel.kuznetsov@hotmail.com>
2016-01-12 14:17:11 +0200

    v1

---
+++ LinkedList/LinkedList/LinkedList.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections;
+
+namespace LinkedList
+{
+    public class Node
+    {
+        public Node Next { get; set; }
+        public Node Prev { get; set; }
+        public object Data { get; set; }
+        public Node()
+        {
+
+        }
+    }
+
+
+    public class MyLinkedList : IEnumerable
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+        private int size = 0;
+
+        public int Length
+        {
+            get { return size; }
+            set { size = value; }
+        }
+
+
+        //Add node to the last position
+        public void AddLast(object data)
+        {
+            //First run
+            if (Head == null)
+            {
+                Head = Tail = new Node();
+                Head.Data = data;
+                Length++;
+
+            }
+            //Second++ run
+            else
+            {
+                Node newNode = new Node();
+                newNode.Data = data;
+                newNode.Prev = Tail;
+                Tail.Next = newNode;
+                Tail = newNode;
+                Length++;
+            }
+        }
+
+        //Add node to the first position
+        public void AddFirst(object data)
+        {
+            //First run 
+            if (Head == null)
+            {
+                Head = Tail = new Node();
+                Tail.Data = data;
+                Length++;
+            }
+            //Second++ Run
+            else
+            {
+                Node newNode = new Node();
+                newNode.Data = data;
+                newNode.Next = Head;
+                Head.Prev = newNode;
+                Head = newNode;
+                Length++;
+            }
+        }
+
+        //Get [index] element from the list
+        public object ElementAt(int index)
+        {
+            if (index < Length)
+            {
+                Node nodeCursor = Head;
+
+                for (int i = 1; i <= index; i++)
+                {
+                    nodeCursor = nodeCursor.Next;
+                }
+                return nodeCursor.Data;
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        //Insert element into [index] position in the list
+        public void AddAt(int index, object obj)
+        {
+            if (index < Length)
+            {
+                Node nodeCursor = Head;
+                Node prevNode = null;
+                Node nextNode = null;
+
+                for (int i = 1; i <= index; i++)
+                {
+                    nodeCursor = nodeCursor.Next;
+                }
+                prevNode = nodeCursor.Prev;
+                nextNode = nodeCursor;
+                Node newNode = new Node();
+                newNode.Data = obj;
+                newNode.Next = nextNode;
+                newNode.Prev = prevNode;
+                prevNode.Next = newNode;
+                nextNode.Prev = newNode;
+                Length++;
+
+            }
+        }
+
+        //Remove [index] element fomr the list
+        public void RemoveAt(int index)
+        {
+            if (index < Length)
+            {
+                Node nodeCursor = Head;
+                Node prevNode = null;
+                Node nextNode = null;
+
+                for (int i = 1; i <= index; i++)
+                {
+                    nodeCursor = nodeCursor.Next;
+                }
+                prevNode = nodeCursor.Prev;
+                nextNode = nodeCursor.Next;
+                prevNode.Next = nextNode;
+                nextNode.Prev = prevNode;
+                Length--;
+
+            }
+        }
+
+        //Implement IEnumerable
+        public IEnumerator GetEnumerator()
+        {
+            Node iterator = Head;
+            while (iterator.Next != null)
+            {
+                yield return iterator;
+                iterator = iterator.Next;
+            }
+        }
+    }
+ 
+}
---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLinkedList l1 = new MyLinkedList();
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 10; i++)
+            {
+                l1.AddLast(i);
+            }
+            //PrintList(l1);
+
+            //l1.AddAt(5, "INSERTED");
+            //PrintList(l1);
+
+            //l1.RemoveAt(5);
+            //PrintList(l1);
+            Console.ReadLine();
+
+
+            foreach (Node n in l1)
+            {
+                Console.WriteLine(n.Data);
+            }
+            Console.ReadLine();
+
+        }
+
+        private static void PrintList(MyLinkedList l1)
+        {
+            for (int i = 0; i < l1.Length; i++)
+            {
+                Console.WriteLine(l1.ElementAt(i));
+            }
+            Console.ReadLine();
+        }
+    }
+}

5d19a884406f25201aad1998280b22b5461cf119
pavel.kuznetsov <pavel.kuznetsov@hotmail.com>
2016-01-19 15:22:58 +0200

    Consoler Listener class with event raising on "quit" typed.

---
+++ ConsoleListener/ConsoleListener/ConsoleReader.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleListener
+{
+    class ConsoleReader
+    {
+        private string _keyWord;
+
+        public string KeyWord
+        {
+            get { return _keyWord; }
+            set
+            {
+                if (value == "quit")
+                {
+                    OnQuitPressEventArgs args = new OnQuitPressEventArgs();
+                    args.word = value;
+                    if (OnQuitPressEvent != null) OnQuitPressEvent(this, args);
+                }
+                _keyWord = value;
+            }
+        }
+        
+        public delegate void WordReader(object obj, OnQuitPressEventArgs args);
+
+        public event WordReader OnQuitPressEvent;
+
+        public ConsoleReader()
+        {
+            KeyWord = null;
+        }
+
+        public string Read()
+        {
+            KeyWord = Console.ReadLine();
+            while (KeyWord != "quit")
+            {
+                KeyWord = Console.ReadLine();
+            }
+            return KeyWord;
+        }
+
+
+    }
+}
---
+++ ConsoleListener/ConsoleListener/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleListener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleReader r1 = new ConsoleReader();
+            r1.OnQuitPressEvent += OnQuitMethod;
+            r1.Read();
+            
+        }
+
+        private static void OnQuitMethod(object sender, OnQuitPressEventArgs args)
+        {
+            Console.WriteLine($"This class {sender.GetType().Name} just recived {args.word} keyword. Raising an Event");
+            Console.ReadLine();
+        }
+
+        
+    }
+
+   
+}

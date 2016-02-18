d1d4e987025447ef779c1db796249edf6909935a
pavel.kuznetsov <pavel.kuznetsov@hotmail.com>
2016-02-18 15:40:58 +0200

    Console app + initial Linq queries

--- Project.LOSA/LOSA.Console/Program.cs
+++ Project.LOSA/LOSA.Console/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using LOSA.BL;
 using LOSA.DBL;
 
@@ -11,16 +8,31 @@ namespace LOSA.Console
     class Program
     {
         static void Main(string[] args)
-        { 
-            
+        {
+            GetAllFlights();
+            System.Console.ReadLine();
         }
 
-
         static void GetAllFlights()
+
         {
             using (var context = new LOSAContext())
             {
-                
+                var query = context.Flights
+                    .Include(f => f.Captain)
+                    .Include(f => f.Arriwal)
+                    .Include(f => f.Departure)
+                    .Include(f => f.Plane);
+                PrintCurrentFlights(query);
+            }
+        }
+
+        static void PrintCurrentFlights(IEnumerable<Flight> flights)
+        {
+            System.Console.WriteLine("FlightID \tFlightObserver \tDeparture \tArriwal \tPlane");
+            foreach (var flight in flights)
+            {
+                System.Console.WriteLine($"{flight.FlightId} \t{flight.Captain.FirstName} {flight.Captain.FirstName} \t{flight.Departure.Code} \t{flight.Arriwal.Code} \t{flight.Plane.Code}");
             }
         }
     }
